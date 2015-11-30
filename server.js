34506859a4f5db1b1d1463c96a90e23b3badde0a
Kristen Gunn <ohkristen@yahoo.com>
2015-11-30 15:53:49 -0800

    second commit

---
+++ server.js
@@ -0,0 +1,59 @@
+var express       = require('express'),
+    app           = express(),
+    ejs           = require('ejs'),
+    ejsLayouts    = require('express-ejs-layouts'),
+    mongoose      = require('mongoose'),
+    flash         = require('connect-flash'),
+    logger        = require('morgan'),
+    cookieParser  = require('cookie-parser'),
+    bodyParser    = require('body-parser'),
+    session       = require('express-session'),
+    passport      = require('passport'),
+    Router        = require('./routes/route.js'),
+    userRouter    = require('./routes/users.js'),
+    port          = process.env.PORT || 3000,
+    passportConfig = require('./config/passport.js')
+
+    //makes json object available in requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+mongoose.connect('mongodb://localhost/videos', function(err){
+	if(err) return console.log('Cannot connect ')
+	console.log('Connected to MongoDB. Sweet!')
+})
+
+// middleware
+app.use(logger('dev'))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.use('/api', Router)
+
+
+
+// session middleware
+app.use(session({
+	secret: 'boomchakalaka',
+	cookie: {_expires: 6000000},
+  resave: true,
+  saveUninitialized: true
+}))
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
+//root route
+app.get('/', function(req,res){
+	res.render('index')
+})
+
+//users route
+app.use('/', userRouter)
+
+app.listen(port, function(){
+  console.log('server running on port', port)
+})
